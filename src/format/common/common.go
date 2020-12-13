d5e1cc07ab5330566b7cf18ed31182070af6f534
ogama <ogama47500@gmail.com>
2020-12-13 18:14:06 +0100

    WIP

--- src/format/common/common.go
+++ src/format/common/common.go
@@ -1,16 +1,31 @@
 package common
 
-import "github.com/ogama/gogen/src/configuration"
+import (
+	"github.com/ogama/gogen/src/configuration"
+	"strings"
+)
 
 type GeneratedObject struct {
 	Object interface{}
 }
 
-func (g GeneratedObject) GetValue() (result interface{}) {
-	if mapValue, isMap := g.Object.(map[string]interface{}); isMap {
+func (g GeneratedObject) GetValue(path string) (result interface{}, exists bool) {
+	result, exists = g.getValuePathArray(strings.Split(path, "."))
+	return result, exists
+}
 
+func (g GeneratedObject) getValuePathArray(pathArray []string) (result interface{}, exists bool) {
+	if len(pathArray) > 0 {
+		field := pathArray[0]
+		if mapValue, isMap := g.Object.(map[string]interface{}); isMap {
+			if result, exists = mapValue[field]; exists {
+
+			}
+		}
+	} else {
+		exists = false
 	}
-	return result
+	return result, exists
 }
 
 type Format interface {
